a40af0d9dbd9170a607aa1649ceb2de53875fc0c
ilham agung <plato.ilham@gmail.com>
2018-09-04 15:56:53 +0700

    add model menu

---
+++ app/menu_sub_1.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class menu_sub_1 extends Model
+{
+    //
+}
---
+++ app/menu_sub_2.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class menu_sub_2 extends Model
+{
+    //
+}
